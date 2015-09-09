34b2bb2a118923ba8f5596c17f222d79985eea1a
Travis CI <juampi92@gmail.com>
2015-09-09 17:53:03 +0000

    Deploy to GitHub Pages

---
+++ docs/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "GSVPANO.PanoLoader"
+    ],
+    "modules": [
+        "GSVPANO"
+    ],
+    "allModules": [
+        {
+            "displayName": "GSVPANO",
+            "name": "GSVPANO"
+        }
+    ],
+    "elements": []
+} };
+});
