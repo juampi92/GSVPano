544791cc128dfae2a570892154d69c8271c4c9e2
Travis CI <juampi92@gmail.com>
2015-09-11 03:09:18 +0000

    Deploy to GitHub Pages

--- docs/api.js
+++ docs/api.js
@@ -1,12 +1,20 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
-        "GSVPANO.PanoLoader"
+        "EventEmitter",
+        "Pano",
+        "PanoLoader"
     ],
     "modules": [
+        "EventEmitter",
         "GSVPANO"
     ],
     "allModules": [
+        {
+            "displayName": "EventEmitter",
+            "name": "EventEmitter",
+            "description": "https://www.npmjs.com/package/event-emitter"
+        },
         {
             "displayName": "GSVPANO",
             "name": "GSVPANO"
